6393c836d7d7daedf03edbe49ccdb35ca5832dec
imelnath <fiomelnathany@gmail.com>
2023-06-07 17:01:03 +0700

    book create, read, update

--- lib/book.py
+++ lib/book.py
@@ -5,8 +5,7 @@ from config import CREDENTIALS
 
 class Book:
     
-    
-    def get_books():
+    def getall_books():
         try:
             db = psycopg2.connect(host=CREDENTIALS['HOSTNAME'],
                                 port=CREDENTIALS['PORT'],
@@ -19,16 +18,13 @@ class Book:
             data = c.fetchall()
             
             res = []
-            for index in data:
+            for col in data:
                 book = {
-                    "store": index[0],
-                    "book_number": index[1],
-                    "book_name": index[2],
-                    "publication_year": index[3],
-                    "pages": index[4],
-                    "pname": index[5],
-                    "quantity": index[6],
-                    "price": index[7],
+                    "book_number": col[0],
+                    "book_name": col[1],
+                    "publication_year": col[2],
+                    "pages": col[3],
+                    "publisher_name": col[4],
                 }
                 res.append(book)
             
@@ -42,8 +38,50 @@ class Book:
             db.close()
             return f'Error while connecting to PostgreSQL Database: {err}'
         
+    def get_books(store_number):
+        try:
+            db = psycopg2.connect(host=CREDENTIALS['HOSTNAME'],
+                                port=CREDENTIALS['PORT'],
+                                database=CREDENTIALS['DATABASE'],
+                                user=CREDENTIALS['USER'],
+                                password=CREDENTIALS['PASSWORD']
+                                )
+            c = db.cursor()
+            c.execute(f"""
+                        SELECT b.book_number, b.book_name, b.publication_year, b.pages, b.publisher_name
+                        FROM book b
+                            JOIN stock st ON b.book_number=st.book_number
+                            JOIN store s ON st.store_number=s.store_number
+                        WHERE s.store_number={store_number} ORDER BY b.book_name ASC
+                    """)
+            data = c.fetchall()
+            msg = "success"
+            
+            items = []
+            for col in data:
+                book = {
+                    "book_number": col[0],
+                    "book_name": col[1],
+                    "publication_year": col[2],
+                    "pages": col[3],
+                    "publisher_name": col[4],
+                }
+                items.append(book)
+            
+            if len(items) == 0:
+                msg = "This store has no books"
+            
+            c.close()
+            db.close()
+            return items, msg
         
-    def get_book(id):
+        except (psycopg2.Error, psycopg2.DatabaseError) as err:
+            c.close()
+            db.close()
+            return [], f'Error while connecting to PostgitemsQL Database: {err}'
+        
+        
+    def get_book(store_number, book_number):
         try:
             db = psycopg2.connect(host=CREDENTIALS['HOSTNAME'],
                                     port=CREDENTIALS['PORT'],
@@ -52,31 +90,47 @@ class Book:
                                     password=CREDENTIALS['PASSWORD']
                                     )
             c = db.cursor()
-            c.execute(f"""SELECT * FROM book
-                      WHERE book_number={id}""")
+            c.execute(f"""
+                        SELECT b.book_number, b.book_name, b.publication_year, b.pages, b.publisher_name
+                        FROM book b
+	                        JOIN stock st ON b.book_number=st.book_number
+	                        JOIN store s ON st.store_number=s.store_number
+                        WHERE st.store_number={store_number} AND b.book_number={book_number}
+                    """)
             data = c.fetchone()
+            msg = "success"
+            
+            item = []
+            if data != None:
+                book = {
+                        "book_number": data[0],
+                        "book_name": data[1],
+                        "publication_year": data[2],
+                        "pages": data[3],
+                        "publisher_name": data[4],
+                    }
+                item.append(book)
+            else:
+                msg = "Not Found"
             
             c.close()
             db.close()
-            
-            return str(data)
+            return item, msg
         
         except (psycopg2.Error, psycopg2.DatabaseError) as err:
             c.close()
             db.close()
-            return f'Error while connecting to PostgreSQL Database: {err}'
+            return [], f'Error while connecting to PostgitemsQL Database: {err}'
     
     
     def add_book(req):
         
-        store = req['store']
-        book_number = req['book_number']
-        book_name = req['book_name']
-        publication_year = req['publication_year']
-        pages = req['pages']
-        pname = req['pname']
-        quantity = req['quantity']
-        price = req['price']
+        store_number = int(req['store_number'])
+        book_name = str(req['book_name'])
+        publication_year = int(req['publication_year'])
+        pages = int(req['pages'])
+        publisher_name = str(req['publisher_name'])
+        quantity = int(req['quantity'])
         
         try:
             db = psycopg2.connect(host=CREDENTIALS['HOSTNAME'],
@@ -86,16 +140,92 @@ class Book:
                                     password=CREDENTIALS['PASSWORD']
                                     )
             c = db.cursor()
-            c.execute(f"""INSERT INTO book (store, book_number, book_name, publication_year, pages, pname, quantity, price)
-                      VALUES({store}, {book_number}, {book_name}, {publication_year}, {pages}, {pname}, {quantity}, {price})""")
+            c.execute(f"""SELECT book_number, book_name FROM book""")
+            books = c.fetchall()
+            msg = "success"
+            
+            _book_numbers = []
+            _book_names = []
+            for book in books:
+                _book_numbers.append(book[0])
+                _book_names.append(book[1])
+            
+            if not (book_name in _book_names):
+                book_number = max(_book_numbers)+1
+                c.execute(f"""INSERT INTO book (book_number, book_name, publication_year, pages, publisher_name)
+                      VALUES({book_number}, '{book_name}', {publication_year}, {pages}, '{publisher_name}')""")
+                c.execute(f"""INSERT INTO stock (store_number, book_number, quantity)
+                          VALUES({store_number}, {book_number}, {quantity})""")
+            else:
+                msg = "Book already exists"
             
             c.close()
             db.commit()
             db.close()
-            
-            return 'success'
+            return msg
         
         except (psycopg2.Error, psycopg2.DatabaseError) as err:
             c.close()
             db.close()
-            return f'Error while connecting to PostgreSQL Database: {err}'
+            return f'Error while connecting to PostgitemsQL Database: {err}'
+    
+    
+    def delete_book(book_number):
+        try:
+            db = psycopg2.connect(host=CREDENTIALS['HOSTNAME'],
+                                    port=CREDENTIALS['PORT'],
+                                    database=CREDENTIALS['DATABASE'],
+                                    user=CREDENTIALS['USER'],
+                                    password=CREDENTIALS['PASSWORD']
+                                    )
+            c = db.cursor()
+            c.execute(f"""
+                      DELETE FROM stock WHERE book_number={book_number}
+                      """)
+            c.execute(f"""
+                      DELETE FROM book WHERE book_number={book_number}
+                      """)
+            msg = "success"
+            
+            c.close()
+            db.commit()
+            db.close()
+            return msg
+            
+        except (psycopg2.Error, psycopg2.DatabaseError) as err:
+            c.close()
+            db.close()
+            return f'Error while connecting to PostgitemsQL Database: {err}'
+    
+    
+    def edit_book(req, book_number):
+        
+        book_name = str(req['book_name'])
+        publication_year = int(req['publication_year'])
+        pages = int(req['pages'])
+        publisher_name = str(req['publisher_name'])
+        
+        try:
+            db = psycopg2.connect(host=CREDENTIALS['HOSTNAME'],
+                                    port=CREDENTIALS['PORT'],
+                                    database=CREDENTIALS['DATABASE'],
+                                    user=CREDENTIALS['USER'],
+                                    password=CREDENTIALS['PASSWORD']
+                                    )
+            c = db.cursor()
+            c.execute(f"""
+                      UPDATE book
+                      SET book_name='{book_name}', publication_year={publication_year}, pages={pages}, publisher_name='{publisher_name}'
+                      WHERE book_number={book_number}
+                      """)
+            msg = "success"
+            
+            c.close()
+            db.commit()
+            db.close()
+            return msg
+        
+        except (psycopg2.Error, psycopg2.DatabaseError) as err:
+            c.close()
+            db.close()
+            return f'Error while connecting to PostgitemsQL Database: {err}'
